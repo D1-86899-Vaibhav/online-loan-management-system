3dec0c66f338c227e7cb55e5fcc4cf9be0536249
ganesh-pawar-02 <ganeshhpawar2002@gmail.com>
2025-02-14 12:30:35 +0530

    removed unnecessary pages

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -3,18 +3,7 @@ import './index.css';
 import * as ReactDOM from 'react-dom/client';
  
 import App from './App';
-import Layout from './layouts/Layout';
-import AuthLayout from './auth/Layout';
-import Dashboard from './pages/Dashboard';
-import Client from './pages/Client';
-import Loans from './pages/Loans';
-import Emi from './pages/Emi';
-import Calculator from './pages/Calculator';
-import Contact from './pages/Contact';
-import Users from './pages/Users';
- 
-import Login from './auth/Login';
-import Register from './auth/Register';
+
 import {BrowserRouter} from 'react-router-dom'
  
 
